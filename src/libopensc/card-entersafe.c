2119d49b2f2f29c33c72fd94a4b13628331ae851
alediator <adiaz@emergya.com>
2012-06-25 13:54:35 +0200

    Added pin cmd function

--- src/libopensc/card-entersafe.c
+++ src/libopensc/card-entersafe.c
@@ -176,6 +176,30 @@ static int entersafe_cipher_apdu(sc_card_t *card, sc_apdu_t *apdu,
 	 SC_FUNC_RETURN(card->ctx, SC_LOG_DEBUG_VERBOSE, SC_SUCCESS);
 }
 
+static int entersafe_gen_random(sc_card_t *card,u8 *buff,size_t size)
+{
+	 int r=SC_SUCCESS;
+	 u8 rbuf[SC_MAX_APDU_BUFFER_SIZE]={0};
+	 sc_apdu_t apdu;
+
+	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+   
+	 sc_format_apdu(card,&apdu,SC_APDU_CASE_2_SHORT,0x84,0x00,0x00);
+	 apdu.resp=rbuf;
+	 apdu.le=size;
+	 apdu.resplen=sizeof(rbuf);
+
+	 r=sc_transmit_apdu(card,&apdu);
+	 SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "entersafe gen random failed");
+
+	 if(apdu.resplen!=size)
+		  SC_FUNC_RETURN(card->ctx, SC_LOG_DEBUG_NORMAL,SC_ERROR_INTERNAL);
+	 memcpy(buff,rbuf,size);
+
+	 SC_FUNC_RETURN(card->ctx, SC_LOG_DEBUG_NORMAL,r);
+}
+
+
 static int entersafe_mac_apdu(sc_card_t *card, sc_apdu_t *apdu,
 							  u8 * key,size_t keylen,
 							  u8 * buff,size_t buffsize)
@@ -528,6 +552,63 @@ static int entersafe_compute_signature(sc_card_t *card,
 	 return entersafe_compute_with_prkey(card,data,datalen,out,outlen);
 }
 
+static void entersafe_init_pin_info(struct sc_pin_cmd_pin *pin, unsigned int num)
+{
+	pin->encoding   = SC_PIN_ENCODING_ASCII;
+	pin->min_length = 4;
+	pin->max_length = 16;
+	pin->pad_length = 16;
+	pin->offset     = 5 + num * 16;
+	pin->pad_char   = 0x00;
+}
+
+static int entersafe_pin_cmd(sc_card_t *card, struct sc_pin_cmd_data *data,
+			   int *tries_left)
+{
+	 int r;
+	 SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+	 entersafe_init_pin_info(&data->pin1,0);
+	 entersafe_init_pin_info(&data->pin2,1);
+	 data->flags |= SC_PIN_CMD_NEED_PADDING;
+
+	 if(data->cmd!=SC_PIN_CMD_UNBLOCK)
+	 {
+		  r = iso_ops->pin_cmd(card,data,tries_left);
+		  sc_debug(card->ctx, SC_LOG_DEBUG_NORMAL, "Verify rv:%i", r);
+	 }
+	 else
+	 {
+		  {/*verify*/
+			   sc_apdu_t apdu;
+			   u8 sbuf[0x10]={0};
+
+			   memcpy(sbuf,data->pin1.data,data->pin1.len);
+			   sc_format_apdu(card, &apdu, SC_APDU_CASE_3_SHORT,0x20,0x00,data->pin_reference+1);
+			   apdu.lc = apdu.datalen = sizeof(sbuf);
+			   apdu.data = sbuf;
+
+			   r = entersafe_transmit_apdu(card, &apdu,0,0,0,0);
+			   SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "APDU transmit failed");
+		  }
+
+		  {/*change*/
+			   sc_apdu_t apdu;
+			   u8 sbuf[0x12]={0};
+
+			   sbuf[0] = 0x33;
+			   sbuf[1] = 0x00;
+			   memcpy(sbuf+2,data->pin2.data,data->pin2.len);
+			   sc_format_apdu(card, &apdu, SC_APDU_CASE_3_SHORT,0xF4,0x0B,data->pin_reference);
+			   apdu.cla = 0x84;
+			   apdu.lc = apdu.datalen = sizeof(sbuf);
+			   apdu.data = sbuf;
+
+			   r = entersafe_transmit_apdu(card, &apdu,key_maintain,sizeof(key_maintain),1,1);
+			   SC_TEST_RET(card->ctx, SC_LOG_DEBUG_NORMAL, r, "APDU transmit failed");
+		  }
+	 }
+	 SC_FUNC_RETURN(card->ctx, SC_LOG_DEBUG_VERBOSE, r);
+}
 static struct sc_card_driver * sc_get_driver(void)
 {
 	struct sc_card_driver *iso_drv = sc_get_iso7816_driver();
@@ -549,6 +630,9 @@ static struct sc_card_driver * sc_get_driver(void)
 	entersafe_ops.set_security_env  = entersafe_set_security_env;
 	entersafe_ops.decipher = entersafe_decipher;
 	entersafe_ops.compute_signature = entersafe_compute_signature;
+
+	/* iso7816-9 functions */
+	entersafe_ops.pin_cmd = entersafe_pin_cmd;
 
 	return &entersafe_drv;
 }
